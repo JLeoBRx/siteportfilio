47bdee523055373ea08023070b986d623f6ab275
Jleo <jleobrmartins@gmail.com>
2025-02-15 21:38:13 -0300

    a

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -27,9 +27,9 @@ export const LoadingScreen = (props) => {
             width: `${progress}%`,
           }}
         >
-          JLeo Dev XR
+          Wawa Sensei
         </div>
-        <div className="opacity-40">JLeo Dev XR</div>
+        <div className="opacity-40">Wawa Sensei</div>
       </div>
     </div>
   );
